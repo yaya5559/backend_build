b11956f2e1b8de5055cb4719db3681b9de3825bf
yaya5559 <ouchcheny@spu.edu>
2025-04-07 00:30:42 -0700

    change secret to JWT_SECRET

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,7 @@ const authMiddleware = (req, res, next) =>{
     if(!token) return res.status(401).json({message: "Unauthorized: No token provided"});
 
     try{
-        const decoded = jwt.verify(token, process.env.JWT_secret);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user  = decoded;
         next();
     }catch (err) {
